d16854fc81372b077059ea4a454ee609fa7726cb
Gitea <gitea@fake.local>
2022-01-28 11:27:28 +0330

    Replaces api.eltak.ir with api-ats.derAlgo.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.eltak.ir/api',
+  baseUrl: 'https://api-ats.deralgo.com/api',
   recaptcha: {
     siteKey: '6LdyXuMdAAAAAG8yEBXM6KbdZyIiTF9_K2M7mMg1',
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:5001/api',
-  // baseUrl: 'https://api.eltak.ir/api',
+  // baseUrl: 'https://localhost:5001/api',
+  baseUrl: 'https://api-ats.deralgo.com/api',
   recaptcha: {
     siteKey: '6LdyXuMdAAAAAG8yEBXM6KbdZyIiTF9_K2M7mMg1',
   }
